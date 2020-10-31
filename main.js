cc289bc13af50bf7c466cddf2379dbaee587de87
Lampenkap007 <59652518+Lampenkap007@users.noreply.github.com>
2020-10-31 23:09:31 +0100

    Add cards for content

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ function tab1(){
   document.getElementById("tab2").style.opacity = "25%";
   document.getElementById("tab3").style.opacity = "25%";
   document.getElementById("title").innerHTML = "To-Do List";
-  document.getElementById("screen2").style.display = "none";
-  document.getElementById("screen3").style.display = "none";
-  document.getElementById("screen1").style.display = "block";
+  document.getElementById("screen2").style.visibility = "hidden";
+  document.getElementById("screen3").style.visibility = "hidden";
+  document.getElementById("screen1").style.visibility = "visible";
 }
 function tab2(){
   document.getElementById("tab1").style.opacity = "25%";
   document.getElementById("tab2").style.opacity = "100%";
   document.getElementById("tab3").style.opacity = "25%";
   document.getElementById("title").innerHTML = "New Item";
-  document.getElementById("screen1").style.display = "none";
-  document.getElementById("screen3").style.display = "none";
-  document.getElementById("screen2").style.display = "block";
+  document.getElementById("screen1").style.visibility = "hidden";
+  document.getElementById("screen3").style.visibility = "hidden";
+  document.getElementById("screen2").style.visibility = "visible";
 
 }
 function tab3(){
@@ -27,8 +27,10 @@ function tab3(){
   document.getElementById("tab2").style.opacity = "25%";
   document.getElementById("tab3").style.opacity = "100%";
   document.getElementById("title").innerHTML = "Settings";
-  document.getElementById("screen2").style.display = "none";
-  document.getElementById("screen1").style.display = "none";
-  document.getElementById("screen3").style.display = "block";
+  document.getElementById("screen2").style.visibility = "hidden";
+  document.getElementById("screen1").style.visibility = "hidden";
+  document.getElementById("screen3").style.visibility = "visible";
 }
+
+
 
