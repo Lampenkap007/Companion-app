bf11a704f9f2511760f41c0342095fbbe53ad468
Jordi <jordifranssenmij@gmail.com>
2020-11-11 15:07:40 +0100

    Implement NodeJS

--- public/main.js
+++ public/main.js
@@ -32,5 +32,16 @@ function tab3(){
   document.getElementById("screen3").style.visibility = "visible";
 }
 
-
+// Logic for saving form
+function saveForm(){
+  if (document.getElementById("formTitle").value == ""){
+    alert("Title required")
+  }
+  else{
+      const form = {title: document.getElementById("formTitle").value, deadline: document.getElementById("formDeadline").value, description: document.getElementById("formDescription").value};
+  console.log(form)
+ document.getElementById("screen1").innerHTML = "<div class='card'><div>   <h1 id='cardTitle'>" + form.title + "</h1>   <h3 id='cardDeadline'>"+ form.deadline + "</h3>   <p id='cardDescription'>"+ form.description + "</p></div><div class='editDelete'><i class='fas fa-edit'></i><i class='far fa-trash-alt'></i></div></div>"
+tab1()
+}
+}
 
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3333
+app.use(express.static('public'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.listen(port, () => {
+  console.log(`Companion-app listening at http://192.168.1.167:${port}`)
+})
